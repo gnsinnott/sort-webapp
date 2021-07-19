f9b670ba84a7035b4da5eb6175c811b0d157370a
gnsinnott <gnsinnott@gmail.com>
2021-07-19 16:53:54 -0500

    moved javascrip to static file

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -12,3 +12,29 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+function delete_record() {
+    var urlArray = window.location.pathname.split('/');
+    var data = {'id' : urlArray[2] };
+    var response = confirm("Are you sure you want to delete this record?")
+    if (response=true) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', "/api/v1/delete_record", true);
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var res = JSON.parse(xhr.response);
+                console.log(res);
+                if (res.status == "true") {
+                    document.getElementById("status").innerHTML = "Record has been deleted.";
+                }
+                else {
+                    document.getElementById("status").innerHTML = "Record could not be deleted.";
+                }
+            }
+        };
+        xhr.send(JSON.stringify(data));
+        return false;
+    }
+}
